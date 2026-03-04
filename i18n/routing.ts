396754228ecf1483e28cecfde953ae4a79b305ec
ValonXh <144491449+LoncheMad@users.noreply.github.com>
2026-03-04 22:31:45 +0100

    Set Albanian as default locale

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,6 +2,6 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   locales: ["en", "al", "mk"],
-  defaultLocale: "en",
+  defaultLocale: "al",
   localePrefix: "always",
 });
